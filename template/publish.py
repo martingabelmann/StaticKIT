fb36f996dc62ef0eb0ef83d778090dfd1c3a59b2
Martin Gabelmann <martin@gabelmann.biz>
2016-03-28 01:44:36 +0200

    support ansible-like {{vars}} in config.yml

--- template/publish.py
+++ template/publish.py
@@ -3,19 +3,56 @@
 from jinja import template
 import yaml
 import time
+import re
+
+# allow/support basic variable substitions in yaml file
+# you can reuse variables with {{variablename}} within other objects
+def parse_vars(yamlin, recursive=False):
+    # save the initial object
+    if not recursive:
+        global global_yaml
+        global_yaml = yamlin
+
+    # recursively go through the object
+    if isinstance(yamlin, list):
+        yamlout=[]
+        for sub in yamlin:
+            yamlout.append(parse_vars(sub, True))
+    elif isinstance(yamlin, dict):
+        yamlout={}
+        for sub in yamlin:
+            yamlout[sub] = parse_vars(yamlin[sub], True)
+
+    # replace {{vars}} in strings with their values
+    elif isinstance(yamlin, str):
+        foundvars=re.findall('{{(.*?)}}', yamlin)
+        if len(foundvars)>0:
+            yamlout=''
+            for var in foundvars:
+                inline_var = '{{' + var + '}}'
+                yamlin = yamlin.replace(inline_var, global_yaml[var])
+            yamlout = yamlin
+        else:
+            yamlout = yamlin
+
+    # be sure nothing gets lost
+    else:
+        yamlout = yamlin
+
+    return yamlout
+
+
 
 try: 
     stream = open('config.yml', 'r')
 except:
     print('could not open config.yml')
 
-
 try:
     rules = yaml.load(stream)
-except:
-    print('config.tml seems not to be a valid yaml file.')
-
-import time
+    rules = parse_vars(rules)
+exept:
+    print('config.yml seems not to be a valid yaml file.')
 
 rules.update({'date':time.strftime("%d/%m/%Y")})
 
